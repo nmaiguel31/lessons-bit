d9f5dd5085aad46aab4a28e2300c7bfc48d1fba1
Nicolas Maiguel <nmaigueluni@gmail.com>
2025-04-22 21:54:14 -0500

    [update] added javascript

---
+++ lesson-javascript/js/script.js
@@ -0,0 +1,71 @@
+/* 
+COMENTARIO
+MULTI LINEA
+*/
+
+// COMENTARIO DE UNA SOLA LINEA
+
+
+/* VARIABLES */
+
+/*
+// Automatica
+nombreVariable1 = 'automatica';
+nombreVariable1 = '1';
+console.log(nombreVariable1);
+
+// var
+var nombreVariable2 = 'usando "var"';
+nombreVariable2 = '2';
+console.log(nombreVariable2);
+
+// let
+let nombreVariable3 = "usando 'let'";
+nombreVariable3 = '3';
+console.log(nombreVariable3)
+
+// const
+const nombreVariable4 = 'usando "const"'
+console.log(nombreVariable4)
+*/
+
+/* OPERADORES */
+// Asignacion
+
+/* TIPOS DE DATOS PRIMITIVOS */
+// undefined
+let miVariable = undefined;
+// String, Cadena de texto
+miVariable = 'usando comillas sensillas';
+miVariable = "usando comillas dobles";
+miVariable = `usando backtips`;
+//number
+miVariable = 0;
+miVariable = -10;
+miVariable = 10;
+miVariable = 0b01;
+miVariable = 0o01;
+miVariable = 0x01;
+miVariable = 0.1;
+//boolean
+miVariable = true;
+miVariable = false;
+console.log('miVariable:', miVariable);
+
+const nombre = 'Nicolas';
+const apellido = 'Maiguel';
+let edad = 23
+
+console.log('Soy', nombre, apellido, "y tengo", edad,"años.");
+/* 
+Crear 3 constantes
+- nombre
+- apellido
+- edad
+imprima en la consola su nombre completo y edad usando los valores de las constantes
+Ej de salida: Soy Pepita perez y tengo 32 anos
+
+- cumple
+- cumple parcialmente
+- no cumple
+*/
